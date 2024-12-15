bfaadd33c873aba37ecb242fb94161ec9c6cdc75
stharvey <simon.harvey.bsl@gmail.com>
2024-12-15 14:09:22 +0000

    Init

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,66 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import MapLibreGL from '@maplibre/maplibre-react-native';
+import { TouchableOpacity } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+MapLibreGL.setAccessToken(null);
 
-export default function HomeScreen() {
+export default function App() {
+
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <View style={styles.page}>
+      <MapLibreGL.MapView
+        style={styles.map}    
+      >
+        <MapLibreGL.Camera
+          zoomLevel={14}
+          centerCoordinate={[-122.48348236083984, 37.83317489144141]}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+
+        <MapLibreGL.MarkerView
+          id="markerId"
+          coordinate={[-122.48348236083984, 37.83317489144141]}
+          style={styles.markerOuterStyle}
+        >
+          <TouchableOpacity onPress={() => console.log('Marker clicked')}>
+          <View style={styles.markerContainer}>
+            <Text style={styles.markerText}>★</Text>
+            </View>
+          </TouchableOpacity>
+        </MapLibreGL.MarkerView>
+
+      </MapLibreGL.MapView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  page: {
+    flex: 1
+  },
+  map: {
+    flex: 1
+  },
+  markerOuterStyle: {
+    width: 46
+  },
+  markerContainer: {
+    backgroundColor: 'white',
+    borderRadius: 25,
+    borderWidth: 4,
+    borderColor: 'red',
+    width: 46,
+    height: 46,
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
+    // Try adding a high zIndex if needed
+    zIndex: 9999,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  markerText: {
+    fontSize: 23,
+    lineHeight: 38,
+    color: 'black',
+    textAlign: 'center',
+  }
 });
